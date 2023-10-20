8d74a0ce013d04e55af711a8b5e6726a964dac7b
DeepakJ99 <36583641+DeepakJ99@users.noreply.github.com>
2023-10-20 10:56:19 +0530

    First Commit

---
+++ src/test/java/com/example/CRUD_REACT/CrudReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUD_REACT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
